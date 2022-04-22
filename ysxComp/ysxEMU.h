5417238ccfcfd23c8aa77351a5db6441924cffc1
Sophia Cristina <43291594+Sophia-Cristina@users.noreply.github.com>
2022-04-22 13:22:35 -0300

    Add files via upload

--- ysxComp/ysxEMU.h
+++ ysxComp/ysxEMU.h
@@ -41,8 +41,19 @@ EPROM DESIGN:
  [E1] https://pdf1.alldatasheet.com/datasheet-pdf/view/27266/TI/PAL16L8.html
 7400-SERIES:
  [T1] https://www.ti.com/lit/ds/symlink/sn74ls00.pdf?ts=1627944918289*/
-// #################################################
-// ############## FLIP-FLOPS ############## [FF1]
+
+ // #################################################
+ // ############## ELEC. COMPONENTS ##############
+
+ // ####### BASIC CIRCUIT:
+
+/* BIPOLAR TRANSISTOR (NPN AND PNP):
+NPN: iC--v->oE | PNP: oC--v-<iE
+		iB	   |		 oB
+Same as an 'AND'*/
+uint8_t TrnsBip(uint8_t In, uint8_t B) { return(In & B); }
+ 
+// ####### FLIP-FLOPS: [FF1]
 
 // SET-RESET FLIP-FLOP, RET. Q0 AND Q1:
 // (S) | (Q & (~(R)))
@@ -67,71 +78,103 @@ Set:
 	0 | 1, 1 | 0 = 0, 0:>-|
 Not allowed:
     0 | 1, 0 | 1 = 0, 0:<-| Loop
-
 	S  = 0101 1110
 	R  = 0110 0010
-
-	^  = 0011 1100
-	S&^= 0001 1100
-	R&^= 0010 0000
+	X  = 0011 1100
+	S&X= 0001 1100
+	R&X= 0010 0000
 */
 struct SRNORFF
 {
 	Point<uint8_t> Q = { 0, 0 };
-	void FF(uint8_t S, uint8_t R)
+	void FF(uint8_t S, uint8_t R = 0)
 	{
 		uint8_t X = S ^ R; S &= X; R &= X;
 		Point<uint8_t> NOR = { ~(Q.y | S), ~(Q.x | R) };
 		Q.x = NOR.y; Q.y = NOR.x;
 	}
-	
 };
 
-/* JK Flip-Flop:
-       PR,    CLR,    CLK
-   110011, 001111, 010101
-*/
-class JKFF
+// D-LATCH, USE CLOCK ON 'EN' TO FLIP-FLOP:
+struct DLatch
 {
-public:
-	uint8_t PR = 0, CLR = 0, CLK = 0;
+	Point<uint8_t> Q = { 0, 0 };
+	void FF(uint8_t D, uint8_t EN)
+	{
+		uint8_t iD = ~D;
+		Point<uint8_t> AND = { iD & EN, D & EN };
+		uint8_t X = AND.x ^ AND.y; AND.x &= X; AND.y &= X;
+		Point<uint8_t> NOR = { ~(Q.y | AND.x), ~(Q.x | AND.y) };
+		Q.x = NOR.y; Q.y = NOR.x;
+	}
+};
+
+// JK FLIP-FLOP:
+struct JKFF
+{
 	uint8_t J = 0, K = 0;
 	Point<uint8_t> Q = { 0, 0 };
-	void FF()
+	void FF(uint8_t CLK)
 	{
-		Point<uint8_t> AND = { ((J & CLK) | Q.y) & PR, ((K & CLK) | Q.x) & CLR };
-		Q.x = AND.y; Q.y = AND.x;
-		//J = Q.y; K = Q.x;
+		Point<uint8_t> AND = { Q.y & CLK & J, Q.x & CLK & K };
+		//uint8_t X = AND.x ^ AND.y; AND.x &= X; AND.y &= X; // Maybe i don't need that anymore, since it have the "toggle" option
+		Point<uint8_t> NOR = { ~(Q.y | AND.x), ~(Q.x | AND.y) };
+		Q.x = NOR.y; Q.y = NOR.x;
 	}
 
 };
 
-// #################################################
-// ############## COUNTERS ############## [D3]
+// ####### COUNTERS:
 
 // DECADE COUNTER:
 class DecadeCnt
 {
 public:
-	Point<uint8_t> Q = { 0, 0 };
-	void FF(uint8_t S, uint8_t R)
+	std::array<SRNORFF, 4> FFs;
+	std::array<uint8_t, 8> Os; // Parallel outs (overwrite by function)
+	uint8_t cnt = 0;
+	void FF(uint8_t CLK)
 	{
-		uint8_t X = S ^ R; S &= X; R &= X;
-		Point<uint8_t> NOR = { ~(Q.y | S), ~(Q.x | R) };
-		Q.x = NOR.y; Q.y = NOR.x;
+		FFs[0].FF(CLK, ~CLK);
+		FFs[1].FF(FFs[0].Q.x, ~FFs[0].Q.x);
+		FFs[2].FF(FFs[1].Q.x, ~FFs[1].Q.x);
+		FFs[3].FF(FFs[2].Q.x, ~FFs[2].Q.x);
+		GetPrllOut();
+	}
+	// GET PARALLEL OUT TO 'Os' ARRAY:
+	void GetPrllOut()
+	{
+		LOOP8
+		{
+			Os[n] = 0; uint8_t tmp;
+			for (uint8_t Q = 0; Q < 4; ++Q)
+			{
+				tmp = ((FFs[Q].Q.x >> n) & 1) * pow(2, Q);
+				Os[n] |= tmp;
+				//Os[n] |= FFs[Q].Q.x << bit;
+				/*  [Q]|  >> n |& 1|pow|Os
+				    b6:|1011011| 0 |1:0| 0
+				    d2:|1101001| 0 |2:0| 0
+				    b1:|1011000| 1 |4:4| 1
+				    4d:|0100110| 1 |8:8| 1 = C = 12
+				*/
+			}
+		}
+
 	}
 };
 
 // #################################################
-// ############## PORTS ##############
+// ############## PORTS & PERIPHERALS ##############
 
 struct USB_C { uint8_t CC, D1, D2, RX1, RX2, TX1, TX2; };
 
 // #################################################
-// ############## PAL16L8 EPROM ##############
+// ############## IC ##############
 
-// #################################################
-/* ############## SN7400 FOUR NAND ############## // [T1]
+// ####### PAL16L8 EPROM:
+
+/* ####### SN7400 FOUR NAND: // [T1]
 https://en.wikipedia.org/wiki/7400-series_integrated_circuits
 	  __________
 1A  -|1 #	  14|- 4Y
