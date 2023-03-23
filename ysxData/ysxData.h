40b6341dba1159314d8bd3e4aa51b027f4d941bb
Sophia Cristina <43291594+Sophia-Cristina@users.noreply.github.com>
2023-03-23 00:30:35 -0300

    Add files via upload

---
+++ ysxData/ysxData.h
@@ -0,0 +1,38 @@
+#pragma once
+
+//#include <time.h>
+//#include "ysxCImg.h" // To create terminal user interfaces
+
+#ifndef YSXDATA_H
+#define YSXDATA_H
+
+// #####################
+// ####### By Sophia Cristina
+// ####### All kinds of things related to how data are
+// ####### processed inside the computer.
+// #####################
+
+// #################################################
+/* REFERENCES:
+ [1]
+*/
+
+// #################################################
+// ############## DATA MANIPULATION ##############
+
+
+
+// #################################################
+// ############## PORTS & PERIPHERALS ##############
+
+// uint8_t data[7];
+struct ysxEMU_USB_C { uint8_t CC, D1, D2, RX1, RX2, TX1, TX2; };
+
+// #################################################
+// ############## PROTOCOLS ##############
+
+
+// #################################################
+// #################################################
+// #################################################
+#endif // SCPARSE_
