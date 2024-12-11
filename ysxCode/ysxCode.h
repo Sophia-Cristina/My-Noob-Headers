6fa2cf73bb5afff3a70cc4d0124684110ea1e984
Sophia Cristina <43291594+Sophia-Cristina@users.noreply.github.com>
2024-12-11 08:21:15 -0300

    Add files via upload

---
+++ ysxCode/ysxCode.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef YSXCODE_H
+#define YSXCODE_H
+
+// #####################
+// ####### By Sophia Cristina
+// ####### All kinds of things related codes,
+// ####### be those machine code, assembly, transpiler, pipeing other apps and etc.
+// #####################
+
+#include ysxOpCodeUtils.h
+#include ysxLanguages.h
+#include ysxPipe.h
+
+// #################################################
+/* REFERENCES:
+ [1] 
+*/
+
+// #################################################
+// ##############  ##############
+
+// #################################################
+// #################################################
+// #################################################
+#endif // SCPARSE_
